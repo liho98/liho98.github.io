c12aca516196eba25fd5e021382cd1125e3933e6
liho <tanlh-wa16@student.tarc.edu.my>
2020-05-31 21:41:36 +0800

    chore: init tailwind css

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const host = '0.0.0.0'
+const port = 8080
+
+module.exports = {
+    lintOnSave: false,
+    productionSourceMap: false,
+    outputDir: path.resolve(__dirname, 'dist'),
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/profile/'
+        : '/',
+    devServer: {
+        host,
+        port,
+        public: `${host}:${port}`,
+        disableHostCheck: true,
+        // https: false,
+    }
+}
