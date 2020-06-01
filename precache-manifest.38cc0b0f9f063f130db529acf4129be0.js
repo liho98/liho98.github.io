71fedfe7fef0ca2ce30c478f32c106b443ae925a
liho <tanlh-wa16@student.tarc.edu.my>
2020-06-02 01:26:41 +0800

    deploy

---
+++ precache-manifest.38cc0b0f9f063f130db529acf4129be0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96a54d889c67a6239940",
+    "url": "/css/app.930f937c.css"
+  },
+  {
+    "revision": "d3c17b5f2ab9660a5b65",
+    "url": "/css/home.bdc9b1d1.css"
+  },
+  {
+    "revision": "8c0d8292e946e4a5c4eda34ce02a5921",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96a54d889c67a6239940",
+    "url": "/js/app.64bb4212.js"
+  },
+  {
+    "revision": "d6162612300f65a0b394",
+    "url": "/js/chunk-vendors.8f1306cf.js"
+  },
+  {
+    "revision": "d3c17b5f2ab9660a5b65",
+    "url": "/js/home.0eb04198.js"
+  },
+  {
+    "revision": "858817f18611e387196546144193eaf4",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
